ee1d0b3feab2d8b8b1406184b1e46fc32723436c
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-13 17:58:51 +0000

    where clauses refresher

--- 1-basic-sql/basic-statements.sql
+++ 1-basic-sql/basic-statements.sql
@@ -33,3 +33,49 @@ LIMIT 5;
 SELECT id, account_id, total_amt_usd
 FROM orders
 ORDER BY account_id, total_amt_usd DESC;
+
+/* results are different depending on which column you order by first */
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY total_amt_usd DESC, account_id;
+
+/* 4) Using the WHERE statement, we can display subsets of tables based on conditions that must be met. You can also think of the WHERE command as filtering the data.
+
+This video above shows how this can be used, and in the upcoming concepts, you will learn some common operators that are useful with the WHERE' statement.
+
+Common symbols used in WHERE statements include:
+
+> (greater than)
+
+< (less than)
+
+>= (greater than or equal to)
+
+<= (less than or equal to)
+
+= (equal to)
+
+!= (not equal to) */
+
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 100
+LIMIT 5;
+
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
+
+/* The WHERE statement can also be used with non-numeric data. We can use the = and != operators here. You need to be sure to use single quotes (just be careful if you have quotes in the original text) with the text data, not double quotes.
+
+Commonly when we are using WHERE with non-numeric data fields, we use the LIKE, NOT, or IN operators. We will see those before the end of this lesson! */
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name != 'Exxon Mobil';
