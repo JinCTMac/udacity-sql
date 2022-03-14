7cd4c7f61da64472760785c0cb5ace858cd2ddbc
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-14 15:34:11 +0000

    IN clause

--- 1-basic-sql/logical-operators.sql
+++ 1-basic-sql/logical-operators.sql
@@ -1,4 +1,35 @@
 /* 1) LIKE
 
 The LIKE operator is extremely useful for working with text. You will use LIKE within a WHERE clause. The LIKE operator is frequently used with %. The % tells us that we might want any number of characters leading up to a particular set of characters or following a certain set of characters, as we saw with the google syntax above. Remember you will need to use single quotes for the text you pass to the LIKE operator, because of this lower and uppercase letters are not the same within the string. Searching for 'T' is not the same as searching for 't'. In other SQL environments (outside the classroom), you can use either single or double quotes.
- */
+*/
+
+SELECT *
+FROM orders
+WHERE url = 'www.google.com';
+
+/* would return 0 as the results don't match that specific url, so we use LIKE and % wildcards to get around that */
+
+SELECT *
+FROM orders
+WHERE url LIKE '%google%';
+
+/* select all companies that have names beginning with C */
+
+SELECT *
+FROM accounts
+WHERE name LIKE 'C%';
+
+SELECT *
+FROM accounts
+WHERE name LIKE '%s';
+
+/* 2) IN clause */
+/* The IN operator is useful for working with both numeric and text columns. This operator allows you to use an =, but for more than one item of that particular column. We can check one, two or many column values for which we want to pull data, but all within the same query. In the upcoming concepts, you will see the OR operator that would also allow us to perform these tasks, but the IN operator is a cleaner way to write these queries.*/
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom');
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords');
