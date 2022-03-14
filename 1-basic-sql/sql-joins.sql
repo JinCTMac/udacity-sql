6bf119b1036600cbdcd772d514dfb21ed1543349
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-14 16:25:44 +0000

    databases and joins refresher

---
+++ 1-basic-sql/sql-joins.sql
@@ -0,0 +1,3 @@
+/* Databases */
+
+/* It's important to think about how data will be organised within a database, which data should be split into two separate tables, logical groupings of the data, making sure that eventually querying the data will be quick and efficient */
