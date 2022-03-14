ad19e8a0b2b01a3ace97bd88afc68fb53c0c2c76
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-14 16:47:54 +0000

    primary and foreign keys

--- 1-basic-sql/sql-joins.sql
+++ 1-basic-sql/sql-joins.sql
@@ -26,3 +26,21 @@ SELECT *
 FROM orders
 JOIN accounts
 ON orders.account_id = accounts.id;
+
+/* this query only selects all columns from orders table */
+
+SELECT orders.*
+FROM orders
+JOIN accounts
+ON orders.account_id = accounts.id;
+
+/* selecting a combination of columns from both tables in a single query */
+
+SELECT orders.standard_qty, orders.gloss_qty, orders.poster_qty, accounts.primary_poc, accounts.website
+FROM accounts
+JOIN orders
+ON accounts.id = orders.account_id;
+
+/* 2) Primary keys (PK) and Foreign keys (FK) */
+
+/* Primary Keys exist in every table, and are unique values, normally an ID, that is made unique via auto-increment. Foreign keys are columns in tables that refer to the primary keys of another table. They usually are defined with the name of the table they reference with _id, like region_id. */
