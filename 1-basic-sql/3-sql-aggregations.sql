8976a93a9c1f5f78852c2a50aa585652e0a2d1c1
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-18 10:50:17 +0000

    testing

---
+++ 1-basic-sql/3-sql-aggregations.sql
@@ -0,0 +1,3 @@
+/* Aggregations in SQL are very similar to Excel, and work on column basis not row basis */
+
+/* NULL is important for this as if there are NULL data values within a column, this could mess up aggregation attempts */
