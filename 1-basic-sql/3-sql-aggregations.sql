216ec7fbd4d24b3d0b7859af9a96705c63d35ad0
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-18 10:54:30 +0000

    NULL datatype

--- 1-basic-sql/3-sql-aggregations.sql
+++ 1-basic-sql/3-sql-aggregations.sql
@@ -1,3 +1,17 @@
 /* Aggregations in SQL are very similar to Excel, and work on column basis not row basis */
 
 /* NULL is important for this as if there are NULL data values within a column, this could mess up aggregation attempts */
+
+/* 1) NULL */
+
+/* to find out if you have NULL values in a column, you need to use IS NULL instead of = NULL, as NULL is a data property, not a value */
+
+SELECT *
+FROM orders
+WHERE primary_poc IS NULL;
+
+/* to do the opposite and find out which rows don't have NULL data, use IS NOT NULL */
+
+SELECT *
+from orders
+WHERE primary_poc IS NOT NULL;
