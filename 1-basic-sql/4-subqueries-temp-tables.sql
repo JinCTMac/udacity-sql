51fb7948819fab37722d0838fa60b266b2c29ece
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-21 17:40:18 +0000

    very difficult subquery questions

--- 1-basic-sql/4-subqueries-temp-tables.sql
+++ 1-basic-sql/4-subqueries-temp-tables.sql
@@ -57,7 +57,57 @@ FROM orders);
 SELECT DATE_TRUNC('day', occurred_at) AS day, AVG(standard_qty) standard, AVG(gloss_qty) gloss, AVG(poster_qty) poster, SUM(total_amt_usd)
 FROM orders
 WHERE DATE_TRUNC('month', occurred_at) =
-(SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min_month
-FROM orders)
+  (SELECT DATE_TRUNC('month', MIN(occurred_at)) AS min_month
+  FROM orders)
 GROUP BY day
 ORDER BY day;
+
+SELECT region, sales_rep, MAX(sales_total)
+FROM sales_performance
+  (SELECT s.name sales_rep, r.name region, SUM(o.total_amt_usd) sales_total
+  FROM orders o
+  JOIN accounts a
+  ON o.account_id = a.id
+  JOIN sales_reps s
+  ON a.sales_rep_id = s.id
+  JOIN region r
+  ON s.region_id = r.id
+  GROUP BY s.name, r.name
+  ORDER BY SUM(o.total_amt_usd) DESC) sales_performance
+GROUP BY sales_rep, region;
+
+/* advanced questions on subqueries */
+
+/* finding region with highest sales and its total orders */
+
+SELECT region, MAX(total_sales) sales, total_orders
+FROM
+  (SELECT r.name region, SUM(o.total_amt_usd) total_sales, COUNT(o.*) total_orders
+  FROM orders o
+  JOIN accounts a
+  ON o.account_id = a.id
+  JOIN sales_reps s
+  ON a.sales_rep_id = s.id
+  JOIN region r
+  ON s.region_id = r.id
+  GROUP BY r.name) region_performance
+GROUP BY region, total_orders
+ORDER BY sales DESC
+LIMIT 1;
+
+/* finding out how many acconuts have had more total purchases than the account with the highest number of standard paper purchases */
+
+SELECT a.name account, SUM(o.total) total_orders
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id
+GROUP BY a.name
+HAVING SUM(o.total) >
+  (SELECT MAX(o.standard_qty) standard_amt
+  FROM orders o
+  JOIN accounts a
+  ON o.account_id = a.id
+  GROUP BY a.name
+  ORDER BY MAX(o.standard_qty) DESC
+  LIMIT 1)
+ORDER BY total_orders DESC;
