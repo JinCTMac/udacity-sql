0ece46c0930cc1c2a8b631df01548370e94b17dd
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-25 11:11:27 +0000

    sql data cleaning

--- 1-basic-sql/5-sql-dat-cleaning.sql
+++ 1-basic-sql/5-sql-dat-cleaning.sql
@@ -1,1 +1,3 @@
 /* CLEANING DATA IN SQL */
+
+/* data is normally not in the format you want, so you oftentimes need to clean and reorganise the data to make it into a suitable format for query/analysis - this involves changing data types, removing NULL data, removing anomalies, etc */
