e7946d6fa688ee03851de7b0a80ba04c2b519fb5
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-28 17:56:32 +0100

    ORDER BY and PARTITION BY to represent group by and order by without joins

--- 1-basic-sql/6-window-functions.sql
+++ 1-basic-sql/6-window-functions.sql
@@ -1,9 +1,16 @@
 /* Window Functions */
 
-/* Window functions allow you to compare one row to another without any joins, allowing you to do things like create a running total and comparing rows against another to see if one row is larger than the other, etc */
+/* Window functions allow you to compare one row to another without any joins, allowing you to do things like create a running total and comparing rows against another to see if one row is larger than the other, or doing an aggregation and partioning results across select data like a GROUP BY, see https://www.postgresql.org/docs/9.1/tutorial-window.html, another resource https://blog.sqlauthority.com/2015/11/04/sql-server-what-is-the-over-clause-notes-from-the-field-101/ */
 
-/* the window function below could be described as calcuating the sum of the standard_qty up to a certain point, ordering by date occurred_at */
+/* ex1) the window function below could be described as calcuating the sum of the standard_qty up to a certain point, ordering by date occurred_at */
 
 SELECT standard_qty,
 SUM(standard_qty) OVER (ORDER BY occurred_at) AS running_total
+FROM orders;
+
+/* ex2) we can also make a window function calculate the running total up to the end of each month */
+
+SELECT standard_qty,
+DATE_TRUNC('month', occurred_at) AS date,
+SUM(standard_qty) OVER ( PARTITION BY DATE_TRUNC('month', occurred_at) ORDER BY occurred_at) AS running_total
 FROM orders;
