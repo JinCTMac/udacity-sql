31044bac6b94b037075643a39b9615fb3c3e2a8d
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-30 16:41:57 +0100

    testing

--- 1-basic-sql/useful-sql-statement.sql
+++ 1-basic-sql/useful-sql-statement.sql
@@ -4,7 +4,7 @@ SELECT
 CAST((SELECT COUNT(*) FROM accounts WHERE name LIKE 'A%') AS FLOAT)/COUNT(*)
 FROM accounts;
 
-/* AGGREGATES - Dividing integer result of count over another count to find proportion, then grouping results by week */
+/* AGGREGATES - Dividing integer result of count over another count to find proportion, then grouping results by week, see this post https://stackoverflow.com/questions/9222664/getting-a-count-of-two-different-sets-of-rows-in-a-table-and-then-dividing-them */
 
 SELECT DATE_TRUNC('week', occurred_at) week,
 CAST((SELECT COUNT(*) FROM orders WHERE total > 50) AS FLOAT)/COUNT(*) orders_over_50
