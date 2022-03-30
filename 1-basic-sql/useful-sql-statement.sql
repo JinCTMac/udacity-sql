ea3152f3bf38a808ccebdefd2dcfff5bf1afca1c
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-03-30 15:48:21 +0100

    another dividing count by count but with order by week

--- 1-basic-sql/useful-sql-statement.sql
+++ 1-basic-sql/useful-sql-statement.sql
@@ -1,5 +1,13 @@
-/* Dividing an integer result of a count over another count - aka finding the proportion of the total data */
+/* AGGREGATES - Dividing an integer result of a count over another count - aka finding the proportion of the total data */
 
 SELECT
 CAST((SELECT COUNT(*) FROM accounts WHERE name LIKE 'A%') AS FLOAT)/COUNT(*)
 FROM accounts;
+
+/* AGGREGATES - Dividing integer result of count over another count to find proportion, then grouping results by week */
+
+SELECT DATE_TRUNC('week', occurred_at) week,
+CAST((SELECT COUNT(*) FROM orders WHERE total > 50) AS FLOAT)/COUNT(*) orders_over_50
+FROM orders
+GROUP BY week
+ORDER BY week;
