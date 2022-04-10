1973018bb3a304fc59b2ab9e8dc41e1617ea93c0
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2022-04-10 17:02:41 +0100

    advanced SQL joins and runtime

---
+++ 1-basic-sql/7-advanced-joins.sql
@@ -0,0 +1,7 @@
+/* LESSON 7 - Advanced Joins and Improving Query Runtime */
+
+/* Inner Joins join data from tables where the condition is matched in both tables */
+
+/* LEFT and RIGHT joins work when the condition is matched and all rows from the left or right table respectively regardless of matching or not */
+
+/* FULL outer joins match data from both tables regardless of matching */
